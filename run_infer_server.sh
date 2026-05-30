f92913ad84b4d3692c1baea27649925e1aa40b05
csccsccsccsc <q490422704@qq.com>
2026-05-31 01:51:54 +0800

    Switch to local server from WAN server

--- run_infer_server.sh
+++ run_infer_server.sh
@@ -2,7 +2,7 @@
 IP_ADDRESS_OF_SERVER=$1
 PORT=$2
 
-python -m web_infer_utils.server_wan \
+python -m web_infer_utils.server \
     --config configs/deployment/wan_pretrain_rela_eef6d.yaml \
     --host $IP_ADDRESS_OF_SERVER \
     --port $PORT
